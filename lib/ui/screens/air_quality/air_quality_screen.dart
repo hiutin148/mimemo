4ee54378de9e07c0da22e4f41dad1e40267cd394
hieunt <hieunt@magenest.com>
2025-07-24 14:07:46 +0700

    feat: add air quality screen

---
+++ lib/ui/screens/air_quality/air_quality_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AirQualityScreen extends StatelessWidget {
+  const AirQualityScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
